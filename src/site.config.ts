97db1abd0ca0491a056d80f55714272abf6fa568
bruno palma <bruno.palma@xempus.com>
2024-05-07 03:34:26 +0200

    cleanup

--- src/site.config.ts
+++ src/site.config.ts
@@ -34,10 +34,10 @@ export const menuLinks: { path: string; title: string }[] = [
 		path: "/",
 		title: "Home",
 	},
-	// {
-	// 	path: "/about/",
-	// 	title: "About",
-	// },
+	{
+		path: "/about/",
+		title: "About",
+	},
 	// {
 	// 	path: "/posts/",
 	// 	title: "Blog",
